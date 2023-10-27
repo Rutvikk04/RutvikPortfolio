7142effabdb262dada70f9a2546ad39fe5246f5e
Rutvikkumar Amarcholi <r.amarcholi@gmail.com>
2023-10-27 06:53:54 +0530

    build added

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./RutvikPortfolio",
+  build: {
+    outDir: 'build', // Or any other desired output directory
+  },
   plugins: [react()],
 })
