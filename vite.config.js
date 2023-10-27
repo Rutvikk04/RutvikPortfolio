2099da5b514fe2ec2ff84ead7a9fcacd3554e209
Rutvikkumar Amarcholi <r.amarcholi@gmail.com>
2023-10-27 07:00:50 +0530

    Workflow Added

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./RutvikPortfolio",
+  base: "/RutvikPortfolio/",
   build: {
     outDir: 'build', // Or any other desired output directory
   },
